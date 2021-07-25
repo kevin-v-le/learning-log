de9b5a5de78c67cea9472eafda0965d52baf8ac4
kevin-v-le <iamkevinvle@gmail.com>
2021-07-25 10:46:04 -0700

    add new_entry() view function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -36,3 +36,23 @@ def new_topic(request):
     #display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """form where user can create a new entry for a topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #no data submitted, create a blank form
+        form = EntryForm()
+    else:
+        #POST data submitted, process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    #display a blank or invalid form
+    context = {'topic': topic,'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
