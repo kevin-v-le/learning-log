04b843c7b83af96ac5ac2e938094d8fa60c4711e
kevin-v-le <iamkevinvle@gmail.com>
2021-07-24 21:40:03 -0700

    create a form for journal entries

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text'] #references attribute 'text' in Topic class in models.py
         labels = {'text': ''} #dictionary
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})} #txt area 80 columns instead of 40
 
