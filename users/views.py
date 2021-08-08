0459ff3eef6c3c02b5203862ebe950a7a5b4522d
kevin-v-le <iamkevinvle@gmail.com>
2021-08-08 13:35:43 -0700

    my attempt at writing the view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def register(request):
-#INCOMPLETE
+    """form where new users can register"""
+    if request.method != 'POST':
+        #no data submitted, process data
+        form = UserCreationForm()
+    else:
+        #POST data submitted, process data
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    #display a blank or invalid form
+    context = {'register': register}
+    return render(request, 'registration/register.html', context)
+
