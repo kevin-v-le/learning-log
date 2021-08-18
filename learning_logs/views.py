f17aa9875f926b1088a76b0cdc5584af572609a5
kevin-v-le <iamkevinvle@gmail.com>
2021-08-18 08:23:25 -0700

    restrict access to every view for users only

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -16,6 +16,7 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
@@ -23,6 +24,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """form where user can create a new topic"""
     if request.method != 'POST':
@@ -39,6 +41,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """form where user can create a new entry for a topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -59,6 +62,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic,'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """form where user can edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
